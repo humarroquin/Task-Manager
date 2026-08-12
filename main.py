c3ec3e16667674ffd5860ed6446664c534e4ff15
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-08-12 15:10:15 -0600

    added a user menu for the app

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 def main():
-    print("Hello from task-manager!")
+    is_running = True
+    while is_running:
+        user_choice = input("""What do you want to do?
+
+    1. Add task
+    2. List tasks
+    3. Complete task
+    4. Quit
+
+Select a number: """)
+
+        if user_choice == "1":
+            print("Adding task...")
+
+        elif user_choice == "2":
+            print("Listing tasks...")
+
+        elif user_choice == "3":
+            print("Completing task...")
+
+        elif user_choice == "4":
+            print("Quit...")
+            is_running = False
 
 
 if __name__ == "__main__":
