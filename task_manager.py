4141888835088a74f194f886d138ce32e8c971db
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-08-13 12:30:13 -0600

    add TaskManager

---
+++ task_manager.py
@@ -0,0 +1,12 @@
+from task import Task
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def list_tasks(self):
+        for task in self.tasks:
+            print(task.title)
