ca9c4135b4fb45bba233f75c9f74e3ac7b826087
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-08-14 12:49:08 -0600

    add task completion and deletion

--- task.py
+++ task.py
@@ -2,3 +2,6 @@ class Task:
     def __init__(self, title):
         self.title = title
         self.completed = False
+
+    def mark_complete(self):
+        self.completed = True
--- task_manager.py
+++ task_manager.py
@@ -10,3 +10,28 @@ class TaskManager:
     def list_tasks(self):
         for task in self.tasks:
             print(task.title)
+
+    def delete_task(self, index):
+        del self.tasks[index]
+
+    def complete_task(self, index):
+        self.tasks[index].mark_complete()
+
+# FOR TESTING PURPOSES
+task1 = Task("Learn variables")
+task2 = Task("Learn functions")
+task3 = Task("Learn OOP")
+
+tasks = TaskManager()
+print("\n==============\n")
+tasks.add_task(task1)
+tasks.add_task(task2)
+tasks.add_task(task3)
+tasks.list_tasks()
+print("\n==============\n")
+tasks.delete_task(1)
+tasks.list_tasks()
+print("\n==============\n")
+
+tasks.complete_task(0)
+print(task1.completed)
