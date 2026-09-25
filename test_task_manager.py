13eab8aa5ffb1a679f8f7bb3a4e32fcf55b3d6c0
humarroquin <149089365+humarroquin@users.noreply.github.com>
2026-09-25 12:48:28 -0600

    add test to check appending and content of add task method

---
+++ test_task_manager.py
@@ -0,0 +1,9 @@
+from task import Task
+from task_manager import TaskManager
+
+
+def test_add_task_appends_to_the_list():
+    manager = TaskManager()
+    manager.add_task(Task("write tests"))
+    assert len(manager.tasks) == 1
+    assert manager.tasks[0].title == "write tests"
